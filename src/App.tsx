ce7c23c9abe487385f6656dac4b7d980bf4f3c56
Sabarinath P S <sabarinathps@Sabarinaths-MacBook-Pro.local>
2023-07-17 09:21:45 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HomeScreen from './features/movies/screens/HomeScreen'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+   <HomeScreen />
+    </SafeAreaView>
+   
+   
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/theme/index.ts
@@ -0,0 +1,2 @@
+const primaryColor: string = "020d21";
+const buttonColor: string = "e3ce06";
